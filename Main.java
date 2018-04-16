2ab2400d380db3a65216a06d92c21ab57f2dba36
Yangyang Mao <38309307+momocoyu@users.noreply.github.com>
2018-04-15 17:17:29 -0700

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,28 @@
+package com.company;
+
+import java.util.ArrayList;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+    }
+
+    public int reverse(int x) {
+        ArrayList<Integer> temp =new ArrayList<>();
+        int result;
+        double number=0;
+        while(x!=0){
+            result=x%10;
+            temp.add(result);
+            x/=10;
+        }
+        for(int i=0;i<temp.size();i++){
+            number*=10;
+            number+=temp.get(i);
+        }
+        if(number<=Integer.MAX_VALUE &&number>=Integer.MIN_VALUE)
+            return (int) number;
+        return 0;
+    }
+}
