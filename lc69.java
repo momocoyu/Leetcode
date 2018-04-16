f9d46d262ac2726acf029a253809da5caf42e85a
Yangyang Mao <38309307+momocoyu@users.noreply.github.com>
2018-04-15 19:55:17 -0700

    Create lc69.java

---
+++ lc69.java
@@ -0,0 +1,11 @@
+//Implement int sqrt(int x).
+
+//Compute and return the square root of x.
+
+//x is guaranteed to be a non-negative integer.
+
+class Solution {
+    public int mySqrt(int x) {
+        return (int) Math.sqrt((double)x);
+    }
+}
