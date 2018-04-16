73effe1ce14b183d6cdc680c878b5985c2640b15
Yangyang Mao <38309307+momocoyu@users.noreply.github.com>
2018-04-15 17:33:40 -0700

    Create lc28.java

---
+++ lc28.java
@@ -0,0 +1,7 @@
+//Return the index of the first occurrence of needle in haystack, or -1 if needle is not part of haystack.
+
+class Solution {
+    public int strStr(String haystack, String needle) {
+         return haystack.indexOf(needle);
+    }
+}
