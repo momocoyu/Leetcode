068e25446f0f17aae237829e7e565bece0ccfd7b
Yangyang Mao <38309307+momocoyu@users.noreply.github.com>
2018-07-06 22:34:58 +0800

    Create lc190.java

---
+++ lc190.java
@@ -0,0 +1,15 @@
+public class Solution {
+    // you need treat n as an unsigned value
+    public int reverseBits(int n) {
+        StringBuilder sb=new StringBuilder(Integer.toBinaryString(n));
+
+        while (sb.length()<32){
+           sb.insert(0,0);
+        }
+        sb.reverse();
+    
+       
+        return Integer.parseUnsignedInt(sb.toString(),2);
+        
+    }
+}
