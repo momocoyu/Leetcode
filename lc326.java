c2feb4cef237ebcc69104fa0db640c18925f08be
Yangyang Mao <38309307+momocoyu@users.noreply.github.com>
2018-07-14 13:59:40 +0800

    Create lc326.java

---
+++ lc326.java
@@ -0,0 +1,13 @@
+class Solution {
+    public boolean isPowerOfThree(int n) {
+        // if(n<1) return false;
+        // while(n>1){
+        //     if((n%3)!=0)
+        //         return false;
+        //     n=n/3;
+        // }
+        // return true;
+        
+        return Math.log10(n)/Math.log10(3) % 1==0;
+    }
+}
