61c7de6ad0a726be6836e8af0baade5959df587c
Yangyang Mao <38309307+momocoyu@users.noreply.github.com>
2018-07-21 16:00:04 +0800

    Create lc206.java

---
+++ lc206.java
@@ -0,0 +1,26 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    
+    public ListNode reverseList(ListNode head) {
+        if(head==null || head.next ==null) return head;
+        ListNode p1=head;
+        ListNode p2=head.next;
+        
+        while(p2!=null){
+            ListNode temp=p2.next;
+            p2.next=p1;
+            if(p1==head) p1.next =null;
+            p1=p2;
+            p2=temp;
+
+        }
+        return p1;
+    }
+}
